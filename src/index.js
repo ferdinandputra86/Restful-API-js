abdb8eac7eaebb05a77e8d7645f942afcecb107e
Ferdinand Putra <ferdinandputra86@example.com>
2026-01-01 14:27:30 +0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+const userRoute = require("./router/users");
+app.use("/users", userRoute);
+
+app.get("/", (_req, res) => {
+  res.json({ message: "API jalan" });
+});
+
+
+app.listen(3000, () => {
+  console.log("Server jalan di port 3000");
+});
---
+++ src/router/users.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const router = express.Router();
+const db = require("../db");
+
+router.get("/", async (req, res) => {
+  try {
+    const [users] = await db.query("select * from users");
+    res.json(users);
+  } catch (err) {
+    res.status(500).json(err.message);
+  }
+});
+
+router.post("/", async (req, res) => {
+  const { name } = req.body;
+  if (!name)
+    return res.status(404).json({ message: "tolong isi nama terlebih dahulu" });
+  try {
+    const [result] = await db.query("insert into users (name) values(?)", [
+      name,
+    ]);
+    res.status(201).json({ message: "Nama Berhasil Ditambahkan" });
+  } catch (err) {
+    res.status(500).json(err.message);
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const [result] = await db.query("delete from users where id = ?", [id]);
+    if (result.affectedRows === 0)
+      return res.status(404).json({ message: "id tidak ditemukan" });
+  } catch (err) {
+    res.status(500).json(err.message);
+  }
+
+  res.json({ message: "User berhasil dihapus" });
+});
+
+router.put("/:id", async (req, res) => {
+  const { id } = req.params;
+  const { name } = req.body;
+  try {
+    const [result] = await db.query("update users set name = ? where id = ?", [
+      name,
+      id,
+    ]);
+    if (result.affectedRows === 0)
+      return res.status(404).json({ message: "ID tidak ditemukan" });
+    res.json({ message: "Data sudah diupdate" });
+  } catch (err) {
+    res.status(500).json(err.message);
+  }
+});
+module.exports = router;
