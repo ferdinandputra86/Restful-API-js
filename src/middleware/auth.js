5b77d1873336f285e73f2eb9cf0dae670833c6c7
Ferdinand Putra <ferdinandputra86@example.com>
2026-01-04 20:21:40 +0700

    middle ware auth

---
+++ src/middleware/auth.js
@@ -0,0 +1,21 @@
+const jwt = require("jsonwebtoken");
+
+module.exports = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    return res.status(401).json({ message: "Token tidak ditemukan" });
+  }
+  const token = authHeader.split(" ")[1];
+  if (!token) {
+    return res.status(401).json({ message: "Token tidak valid" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_KEY);
+    req.user = decoded;
+    next();
+  } catch (err) {
+    return res.status(401).json({ messsage: "Token expire" });
+  }
+};
--- src/router/users.js
+++ src/router/users.js
@@ -1,15 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const userController = require("../controller/UserController");
+const auth = require("../middleware/auth");
 
-router.get("/", userController.getUser);
+router.get("/", auth, userController.getUser);
 
 router.post("/", userController.createUser);
 
 router.delete("/:id", userController.deleteUser);
 
 router.put("/:id", userController.updateUser);
-
-
 
 module.exports = router;
